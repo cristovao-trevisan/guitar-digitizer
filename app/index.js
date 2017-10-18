581cc5ea98e0d69769ed4b1f2f07d36015405f4b
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2017-10-18 01:23:38 -0200

    Using local semantic-ui-css to remove remote deps, updating a few deps, using release build

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
+import './css/semantic.css'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
